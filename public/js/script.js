eb6beaeb1d767d4326ff3d452dfd17bcd68d551d
Mateusz Nawrocki <mateusz.nawroo@gmail.com>
2021-06-10 20:45:38 +0200

    add connection in register and login page

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.navigation');
+const icon = document.querySelector(".js-options");
+const logOut = document.querySelector(".logOutInvisible");
 
 
 const handleClick = () => {
@@ -13,5 +15,12 @@ const makeDecision = document.querySelector(".js-makeDecision");
 makeDecision.addEventListener("click", (event) => {
     event.preventDefault();
     location.replace("welcome.html");
+})
+
+console.log(logOut);
+console.log(icon);
+
+icon.addEventListener("click", () => {
+    logOut.classList.toggle("logOutVisible");
 })
 
