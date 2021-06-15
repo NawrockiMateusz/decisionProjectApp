b9d2d0573012e87407e80ae519e4b3d149710dc2
Mateusz Nawrocki <mateusz.nawroo@gmail.com>
2021-06-15 21:46:42 +0200

    default configuration

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController
+{
+
+    public function index()
+    {
+        //TODO display login.html
+    }
+
+    public function start()
+    {
+        //TODO display start.html
+    }
+}
