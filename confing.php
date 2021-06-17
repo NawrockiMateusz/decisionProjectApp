972d52f11f9d39565372b06be5a6964188becd2f
Mateusz Nawrocki <mateusz.nawroo@gmail.com>
2021-06-18 00:29:01 +0200

    add connection with database and create files to pull info from database

---
+++ confing.php
@@ -0,0 +1,7 @@
+<?php
+
+const USERNAME = 'eqzuwtexqusiab';
+const PASSWORD = '3cc036ac92310bf943ba4774918956c195f868c2dba98876118068656e22f52d';
+const HOST = 'ec2-54-155-208-5.eu-west-1.compute.amazonaws.com';
+const DATABASE = 'd2515fltv9fhm9';
+
